460c3f0133fb21b5893aeb8303ca53c5dffe2c1d
Ida Aspen <ida.aspen@outlook.com>
2022-01-19 09:06:49 +0100

    feat: add crypto and bcrypt for password function

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import crypto from 'crypto';
+import bcrypt from 'bcrypt';
 
 const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/finalproject';
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });

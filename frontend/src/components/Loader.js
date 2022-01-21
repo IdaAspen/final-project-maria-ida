d3e442379db416df99e1411e4293522c0bf101d7
Maria Petersson <mariapetersson@Marias-MacBook.lan>
2022-01-21 12:19:53 +0100

    FEAT: new reducer rainbowLoader. REFACTOR: rewrite code in Loader.json

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -3,21 +3,24 @@ import Lottie from 'react-lottie';
 import animationData from '../animations/loader.json';
 import { useSelector } from 'react-redux';
 
-// export const Loader = () => {
-//   const loading = useSelector((store) => store.game.loading); };
+export const Loader = () => {
+  const loading = useSelector((store) => store.rainbowLoader.loading);
 
-const defaultOptions = {
-  loop: true,
-  autoplay: true,
-  animationData,
-  rendererSettings: {
-    preserveAspectRatio: 'xMidYMid slice'
-  }
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
+
+  return (
+    <div className="lottie">
+      {/* <Loader options={defaultOptions} height={400} width={400} /> */}
+      {loading && <Lottie options={defaultOptions} height={400} width={400} />}
+    </div>
+  );
 };
 
-return (
-  <div className="lottie">
-    <Loader options={defaultOptions} height={400} width={400} />
-    {/* {loading && <Lottie options={defaultOptions} height={400} width={400} />} */}
-  </div>
-);
+export default Loader;

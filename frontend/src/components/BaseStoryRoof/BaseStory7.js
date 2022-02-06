fb9b023b6f7a699ba9f4ec991262c93fd26cb942
Maria Petersson <mariapetersson@Marias-MacBook.lan>
2022-02-06 22:06:11 +0100

    add: styling to login wip

--- frontend/src/components/BaseStoryRoof/BaseStory7.js
+++ frontend/src/components/BaseStoryRoof/BaseStory7.js
@@ -31,12 +31,10 @@ const BaseStory7 = () => {
 
   return (
     <div className="base-container">
-      <div className="base-container">
-        <section className="base-story-orange">
-          <p>{`Jo, jag heter ${friendsName}. Vill du ha en nöt av mig? – hej, svarar ${character}. Jättegärna. Jag älskar faktiskt nötter. – Det gör jag med. Och de här låter så roligt när man skalar dem, säger ${friendsName}n. 
+      <section className="base-story-orange">
+        <p>{`Jo, jag heter ${friendsName}. Vill du ha en nöt av mig? – hej, svarar ${character}. Jättegärna. Jag älskar faktiskt nötter. – Det gör jag med. Och de här låter så roligt när man skalar dem, säger ${friendsName}n. 
           – Ja, jag vet, säger ${character} och så skrattar de båda två.`}</p>
-        </section>
-      </div>
+      </section>
 
       <h3>Vill du läsa hela sagan och spara den, eller starta om?</h3>
       <div className="btn-container">
